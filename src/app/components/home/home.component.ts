ef17a2415dd74f4900672d5fb14228910696ea57
Mradroit611 <sanjayvermaa12121@gmail.com>
2024-12-16 08:33:01 +0530

    meta tag added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-// import { HttpClientModule } from '@angular/common/http'; 
-import { Observable } from 'rxjs';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { Title, Meta } from '@angular/platform-browser';
 import * as languagesData from '../../../data/languages.json';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 interface Language {
   image: string;
@@ -24,8 +22,13 @@ export class HomeComponent implements OnInit {
 
   languages: Language[] = (languagesData as any).default;
 
-  constructor() {}
+  constructor(private titleService: Title, private metaService: Meta) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Set the page title and description for SEO
+    this.titleService.setTitle('Welcome to CodeLib - Explore Technologies');
+    this.metaService.updateTag({ name: 'description', content: 'Explore a curated list of technologies and learn about their features and applications. CodeLib provides a quick overview of popular technologies used in modern development.' });
+    this.metaService.updateTag({ name: 'keywords', content: 'programming, technologies, languages, coding, learning, code sharing' });
+  }
 
 }
